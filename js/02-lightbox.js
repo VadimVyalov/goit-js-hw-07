68b87cd256a1ea8a67ff943fcd86e87bb091debf
Vyalov Vadim <vyalov.vadim@gmail.com>
2022-12-28 20:05:00 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,6 @@ import { galleryItems } from "./gallery-items.js";
 
 const makeGalleryItem = ({ preview, original, description }) => {
   return `
-
   <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
@@ -14,16 +13,12 @@ const makeGalleryItem = ({ preview, original, description }) => {
   </a>`;
 };
 
-const galleryList = document.querySelector(".gallery");
 const list = galleryItems.map(makeGalleryItem).join(" ");
+const galleryList = document.querySelector(".gallery");
+
 galleryList.insertAdjacentHTML("afterbegin", list);
 
-let gallery = new SimpleLightbox(".gallery a", {
+const gallery = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
-});
-gallery.on("show.simplelightbox");
-
-gallery.on("error.simplelightbox", function (e) {
-  console.log(e); // some usefull information
 });
