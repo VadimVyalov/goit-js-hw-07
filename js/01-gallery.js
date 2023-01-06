36adf01214f8fe87395115c55d5b3ee8021f6a8c
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-01-06 04:15:39 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -49,16 +49,145 @@ const onCLickImg = (event) => {
     width="800" 
     height="600"
     >`;
-  const instance = basicLightbox.create(htmlContent, {
-    onShow: (instance) => {
-      event.currentTarget.addEventListener("keydown", (event) => {
-        if (event.code === "Escape") instance.close();
-      });
+
+  //! -1- >>> не реализовано снятие событий
+  //   const instance = basicLightbox.create(htmlContent, {
+  //     onShow: (currentInstance) => {
+  //       event.currentTarget.addEventListener("keydown", (event) => {
+  //         if (event.code === "Escape") currentInstance.close();
+  //       });
+  //     },
+  //   });
+  //   instance.show();
+  // };
+  //! -1- <<<
+
+  //! -2- >>> ошибка на снятии события
+
+  //   const instance = basicLightbox.create(htmlContent, {
+  //     onShow: (currentInstance) => {
+  //       event.currentTarget.addEventListener(
+  //         "keydown",
+  //         closeOnShow(currentInstance)
+  //       );
+  //     },
+  //     onClose: (currentInstance) => {
+  //       event.currentTarget.removeEventListener(
+  //         "keydown",
+  //         closeOnShow(currentInstance)
+  //       ); //error
+  //     },
+  //   });
+  //   instance.show();
+  // };
+  // const closeOnShow = function (inst) {
+  //   return function keypress(evt) {
+  //     if (evt.code === "Escape") {
+  //       inst.close();
+  //     }
+  //   };
+  // };
+
+  //! -2- <<<
+
+  //! -3- >>> снимает событие только по ESC, костыль в CallBack
+
+  //   const instance = basicLightbox.create(htmlContent, {
+  //     onShow: (instance) => {
+  //       document.body.addEventListener("keydown", closeOnShow(instance));
+  //     },
+  //     onClose: (instance) => {
+  //       document.body.removeEventListener("keydown", closeOnShow(instance));
+  //     },
+  //   });
+
+  //   instance.show();
+  // };
+
+  // //*========= CallBack >>>>>>>>>
+
+  // const closeOnShow = function (inst) {
+  //   return function keypress(evt) {
+  //     if (evt.code === "Escape") {
+  //       inst.close();
+  //       evt.currentTarget.removeEventListener("keydown", keypress);
+  //       console.log("Escape");
+  //     }
+  //   };
+  // };
+  // //*========= CallBack <<<<<<<<<<
+  //! -3- <<<
+  //! дошло что евент смотрит вникуда. всё дальше работает
+  //! -4- >>> CallBack внутри обработчика, поэтому у всех одна область видимости
+  //   const instance = basicLightbox.create(htmlContent, {
+  //     onShow: () => {
+  //       document.body.addEventListener("keydown", closeShowOn);
+  //     },
+  //     onClose: () => {
+  //       document.body.removeEventListener("keydown", closeShowOn);
+  //     },
+  //   });
+
+  //   instance.show();
+
+  //   function closeShowOn(evt) {
+  //     if (evt.code === "Escape") {
+  //       instance.close();
+  //       console.log("Escape");
+  //     }
+  //   }
+  // };
+  //! -4- <<<
+
+  //! -5- >>> сделано через bind. как работает до конца не понимаю
+  //   const instance = basicLightbox.create(htmlContent, {
+  //     onShow: () => {
+  //       document.body.addEventListener("keydown", ff);
+  //     },
+  //     onClose: () => {
+  //       document.body.removeEventListener("keydown", ff);
+  //     },
+  //   });
+  //   const ff = closeShowOn.bind(null, instance);
+  //   instance.show();
+  // };
+
+  // const closeShowOn = function (inst) {
+  //   if (event.code === "Escape") {
+  //     inst.close();
+  //     console.log("Escape");
+  //   }
+  // };
+  //! -5- <<<
+
+  //! -6- >>> сделано по спецификации, глобальный объект
+
+  evtHandler.instance = basicLightbox.create(htmlContent, {
+    onShow: () => {
+      document.body.addEventListener("keydown", evtHandler);
+    },
+    onClose: () => {
+      document.body.removeEventListener("keydown", evtHandler);
     },
   });
 
-  instance.show();
+  evtHandler.instance.show();
 };
+
+const evtHandler = {
+  inst: null,
+  handleEvent: keydownHandler,
+};
+
+function keydownHandler(evt) {
+  if (evt.code === "Escape") {
+    this.instance.close();
+    console.log("Escape");
+  }
+}
+//! -6- <<<
+
+//! ======== main
 
 const galleryList = document.querySelector(".gallery");
 galleryList.addEventListener("click", onCLickImg);
